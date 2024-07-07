3e0a4a1f05a8f6e1a31bec484930a2f0c07acffd
SebastianBonilla13 <107320080+SebastianBonilla13@users.noreply.github.com>
2024-07-07 15:58:26 -0500

    About me v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { FaInstagram, FaLinkedin } from "react-icons/fa6";
+
+import imgSebastianBonilla from "./Assets/imgSebastianBonilla.jpg";
+
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +14,48 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div>
+          
+          <div>
+            {/* <img src='../src/Assets/imgSebastianBonilla.jpg' alt='SebastianBonilla'></img> */}
+            {/* <img src="/imgSebastianBonilla.jpg"/> */}
+            <img src={imgSebastianBonilla} style={{width: "40%"}}/>
+          </div>
+
+          <div>
+            <h1>
+              Sebastián Bonilla
+            </h1>
+          </div>
+
+          <div>
+            <h2>
+              Software Developer
+            </h2>
+          </div>
+
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </p>
+          </div>
+          
+        </div>
+
+        <div>
+        <FaInstagram />
+        </div>
+
+        <div>
+        <FaLinkedin />
+        </div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
