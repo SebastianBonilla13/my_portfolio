d903d45864e01b2fc3a695d28d2d1fcc6c9a30b6
SebastianBonilla13 <107320080+SebastianBonilla13@users.noreply.github.com>
2024-07-19 23:58:56 -0500

    CardProject v1.1 +little details

--- src/Components/CardProject.jsx
+++ src/Components/CardProject.jsx
@@ -8,40 +8,56 @@ function CardProject() {
 
             <div class="group-hover:h-aut">
 
-            
-            {/*  */}
 
-            <div class="flex justify-center ">
-                <div class="w-2/ w-full group-hover:w-2/3 transition-all duration-700 ease-out">
-                    <img class="" src={imgReportesPlus} /* style={{ width: "40%", paddingTop: "50px" }}  */ />
-                </div>
-            </div>
+                {/*  */}
 
-            <div class="group-hover:h-0">
-            <div class="z-10 inset-0 flex items-center bg-red-30 group-hover:pl-0 group-hover:opacity-0  transition-all duration-300 ease-out">
-                <div class="flex flex-col gap-y-2">
-                    <span class="text-2xl font-bold max-w-[13ch]">Reportes Plus</span>
-                    <p class=" mb-2 w-full"> Información Presupuestal Universidad del Cauca. Proporcionar a los usuarios
-                        finales del área financiera e interesados de la  universidad, una herramienta
-                        que les permita consultar y descargar  información presupuestal en tiempo real.</p>
-                    <div class="flex flex-wrap gap-3">
-                        <Badge Text={"HTML"} > </Badge>
-                        <Badge Text={"Boostrap"} > </Badge>
-                        <Badge Text={"JavaScript"} > </Badge>
-                        <Badge Text={"React"}> </Badge>
+                <div class="flex justify-center  ">
+                    <div class="w-full  group-hover:w-1/2 transition-all duration-700 ease-out">
+                        <img class="" src={imgReportesPlus} /* style={{ width: "40%", paddingTop: "50px" }}  */ />
                     </div>
                 </div>
-            </div>
-            </div>
+
+                <div class="h-0 hidden group-hover:flex group-hover:h-full"> {/* group-hover:h-0 */}
+                    <div class="group-hover:z-10 group-hover:inset-0  group-hover:items-center bg-red-30 roup-hover:pl-0 roup-hover:opacity-0  transition-all duration-300 ease-out"> {/* group-hover:pl-0 group-hover:opacity-0  */}
+                        <div class="flex flex-col gap-y-2">
+                            <span class="text-2xl font-bold ">Reportes Plus</span> {/* max-w-[13ch] */}
+                            <p class=" mb-2 "> Información Presupuestal Universidad del Cauca. Proporcionar a los usuarios {/* w-full */}
+                                finales del área financiera e interesados de la  universidad, una herramienta
+                                que les permita consultar y descargar  información presupuestal en tiempo real.</p>
+                            <div class="flex flex-wrap gap-3">
+                                <Badge Text={"HTML"} > </Badge>
+                                <Badge Text={"Boostrap"} > </Badge>
+                                <Badge Text={"JavaScript"} > </Badge>
+                                <Badge Text={"React"}> </Badge>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
-            
+                {/* <div class="group-hover:h-0">
+                    <div class="z-10 inset-0 flex items-center bg-red-30 group-hover:pl-0 group-hover:opacity-0  transition-all duration-300 ease-out">
+                        <div class="flex flex-col gap-y-2">
+                            <span class="text-2xl font-bold max-w-[13ch]">Reportes Plus</span>
+                            <p class=" mb-2 w-full"> Información Presupuestal Universidad del Cauca. Proporcionar a los usuarios
+                                finales del área financiera e interesados de la  universidad, una herramienta
+                                que les permita consultar y descargar  información presupuestal en tiempo real.</p>
+                            <div class="flex flex-wrap gap-3">
+                                <Badge Text={"HTML"} > </Badge>
+                                <Badge Text={"Boostrap"} > </Badge>
+                                <Badge Text={"JavaScript"} > </Badge>
+                                <Badge Text={"React"}> </Badge>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
 
-            {/* <img class="right-5 absolute w-[250px] group-hover:w-[500px]  group-hover:h-auto transition-all duration-700 ease-out" src="/assets/tensei2-3STPUW1i.webp" alt="Photo Tensei" style="transform: none;"></img>
+
+                {/* <img class="right-5 absolute w-[250px] group-hover:w-[500px]  group-hover:h-auto transition-all duration-700 ease-out" src="/assets/tensei2-3STPUW1i.webp" alt="Photo Tensei" style="transform: none;"></img>
  */}
 
 
-            {/*  */}
+                {/*  */}
 
             </div>
         </div>
