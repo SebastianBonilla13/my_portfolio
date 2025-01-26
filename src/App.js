eec8014d9ee693314cd0a6875507dd7d23232961
Sebastian Bonilla <jsbonilla@unicauca.edu.co>
2025-01-26 03:22:45 -0500

    clone repositoy test

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
-/* import logo from './logo.svg'; */
-
-/* import './App.css'; */
 import React, { useRef } from 'react';
-import { useScroll } from 'react-scroll';
-
-import ToolTip1 from "./Components/ToolTip1.jsx";
-
 
 import Sidebar from "../src/Components/Sidebar.jsx";
-
-/* import Card from "../src/Components/Card.jsx"; */
 import TableInfo from "./Components/TableInfo.jsx";
 import CardProject from "./Components/CardProject.jsx";
+import ToolTip1 from "./Components/ToolTip1.jsx";
 
-import { FaGithub, FaLinkedin, FaRegFolderOpen, FaSuitcase } from "react-icons/fa6";
-import { FaSmileWink, FaClipboardList } from "react-icons/fa";
-import { IoMdPerson } from "react-icons/io";
-
-import { FaSearchengin } from 'react-icons/fa'
-
-// en sidebar, (quitar luego)
-/* FaRegFolderOpen
-FaSuitcase
-FaSmileWink
-IoMdPerson */
-
-import Badge from "../src/Components/Badge.jsx";
-
+import { FaGithub, FaLinkedin} from "react-icons/fa6";
 
 import imgSebastianBonilla from "./Assets/imgSebastianBonillaa.jpg";
 import imgReportesPlus from "./Assets/imgReportesPlus.png";
+import imgEncriptadorTexto from "./Assets/imgEncriptadorTexto.png";
 
 /* listado tecnologias Proyectos */
-let lstTechP1 = ["HTML", "CSS", "JavaScript", "React"];
+let lstTechP1 = ["Bootstrap", "JavaScript", "React"];
+let lstTechP2 = ["Tailwind CSS", "JavaScript", "React"];
 
 /* listado tecnologias Experiencia */
 let lstTechE1 = ["C", "C++", "C#", "HTML", "Java", "JavaScript", "Python", "SQL"];
 let lstTechE2 = ["HTML", "CSS", "JavaScript", "Tailwind CSS", "React", "Figma", "Git"];
-
-/* let lstT = ["Hola", "soy", "una", "etiqueta"]; */
-
 
 
 function App() {
@@ -48,34 +26,23 @@ function App() {
 
     <div class="flex justify-between static bg-gradient-to-r from-slate-800  to-slate-900 "> {/* STRUCT */}
 
-
       <header class="hidden lg:flex justify-center 2xl:w-3/5 2xl:justify-end lg:w-1/3 bg-color_primary lg:sticky lg:top-0 right-0  lg:max-h-screen w-max"> {/* SIDEAR */}  {/* bg-color_primary  lg:flex  lg:max-h-screen */}
         <div class="flex justify-center">
           <Sidebar> </Sidebar>
         </div>
-
-
-
-
       </header>
-
 
       <main class="lg:w-2/3 2xl:w-3/4 "> {/* BODY */} {/* bg-lime-200 */}
 
-        {/* <Badge Icon={FaSmileWink} Text={"HOLA"}> </Badge> */}
-        {/* <h1 class="[text-shadow:_0_1px_0_rgb(0__2_/_40%)]">Hello</h1> */}
-
-        <div id="sobremi" class="lg:max-w-4xl px-6 sm:px-20 pt-10">
+        <div id="sobremi" class="lg:max-w-4xl px-6 sm:px-20 pt-10 ">
 
           {/* section SOBRE MI */}
-          <div class="mb-60">
+          <div class="mb-60 ">
             {/* IMAGE CONTAINER */}
-            {/* ring-1 ring-white ring-opacity-20 shadow-lg shadow-indigo-100/10 hover:shadow-indigo-100/25 */}
 
-            <div class="flex justify-center bg-red-400">
+            <div class="flex justify-center pb-60">
 
               {/* LUZ ESTADO */}
-
               <div class="absolute z-40 mt-36 ml-44 pl-3">
                 <ToolTip1 position="bottom" tooltipsText="Disponible ;)">
                   <span class="relative flex h-4 w-4 ">
@@ -85,9 +52,8 @@ function App() {
                 </ToolTip1>
               </div>
 
-
-
-              <div class="absolute z-20">
+              {/* IMAGE */}
+              <div class="absolute z-20 ">
                 <div class="flex justify-center">
                   <div class="w-52 rounded-full overflow-hidden ring-2 ring-slate-300 shadow-xl shadow-teal-300/35 over:shadow-slate-1/100 ">
                     <img src={imgSebastianBonilla} />
@@ -97,41 +63,13 @@ function App() {
 
             </div>
 
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-
-
-            {/* <span class="relative flex h-3 w-3 bg-red-400">
-              <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-              <span class="relative inline-flex rounded-full h-3 w-3 bg-green-400"></span>
-            </span>
-
-            <div class="flex justify-center pt-5 ">
-              <div class="w-52 order-solid order-color_primary order-slate-200 order-8 rounded-full overflow-hidden  ring-8 ring-slate-300 ing-opacity-20 shadow-xl shadow-slate-100/30 over:shadow-slate-1/100">
-                <img src={imgSebastianBonilla} />
-              </div>
-            </div> */}
-
             {/* ICONS CONTAINER */}
             <div class="flex justify-center">
               <div class="pt-1 pb-6 gap-x-5 grid grid-cols-2 grid-flow-row">
 
                 <div class="p-5">
-
-                  {/* <div class="p-5 bg-transparent  w-10 h-10 rounded-full inline-flex items-center justify-center  border-2 border-slate-200 shadow-lg shadow-slate-300/15 hover:shadow-teal-300/30">
-                    <a class=" p-1" href='https://www.linkedin.com/in/sebastianbonilla13'>
-                      <FaLinkedin color="#e2e8f0" class="w-10 h-10 p-2" />
-                    </a>
-                  </div> */}
-
+                  
+                  {/* ICON LinkedIn */}
                   <ToolTip1 position="bottom" tooltipsText="LinkedIn">
                     <div class="p-5 bg-transparent  w-10 h-10 rounded-full inline-flex items-center justify-center  border-2 border-slate-200 shadow-lg shadow-slate-300/15 hover:shadow-teal-300/30">
                       <a class=" p-1" href='https://www.linkedin.com/in/sebastianbonilla13'>
@@ -142,15 +80,8 @@ function App() {
 
                 </div>
 
-
-                {/* ICON */}
+                {/* ICON GitHub */}
                 <div class="p-5">
-
-                  {/* <div class="p-5 bg-transparent  w-10 h-10 rounded-full inline-flex items-center justify-center  border-2 border-slate-200 shadow-lg shadow-slate-300/15 hover:shadow-teal-300/30">
-                    <a class=" p-1" href='https://www.linkedin.com/in/sebastianbonilla13'>
-                      <FaLinkedin color="#e2e8f0" class="w-10 h-10 p-2" />
-                    </a>
-                  </div> */}
 
                   <ToolTip1 position="bottom" tooltipsText="GitHub">
                     <div class="p-5 bg-transparent  w-10 h-10 rounded-full inline-flex items-center justify-center  border-2 border-slate-200 shadow-lg shadow-slate-300/15 hover:shadow-teal-300/30">
@@ -160,33 +91,7 @@ function App() {
                     </div>
                   </ToolTip1>
 
-
                 </div>
-
-
-
-                {/* <div class="p-5 bg-transparent  w-10 h-10 rounded-full inline-flex items-center justify-center  border-2 border-slate-200 shadow-lg hover:shadow-teal-300/35">
-                  <a class=" p-1" href='https://www.linkedin.com/in/sebastianbonilla13'>
-                    <FaLinkedin color="#e2e8f0" class="w-10 h-10 p-2" />
-                  </a>
-                </div> */}
-
-
-
-                {/* <div class="p-5"> */}
-                {/* <div class="w-10 border-opacity h-10 rounded-full inline-flex items-center justify-center relative z-10 border-solid border-2 border-slate-200">
-                    <a class=" " href='https://github.com/SebastianBonilla13'>
-                      <FaGithub color="#e2e8f0" class="w-10 h-10 p-2 zinc-700" />
-                    </a>
-                  </div> */}
-                {/* <div class="p-5 bg-transparent  w-10 h-10 rounded-full inline-flex items-center justify-center  border-2 border-slate-200 shadow-lg shadow-slate-300/15 hover:shadow-teal-300/30">
-                    <a class=" p-1" href='https://github.com/SebastianBonilla13'>
-                      <FaGithub color="#e2e8f0" class="w-10 h-10 p-2" />
-                    </a>
-                  </div> */}
-
-                {/* </div> */}
-
               </div>
             </div>
 
@@ -195,39 +100,14 @@ function App() {
 
               {/* NAME */}
               <div class="text-left">
-                {/* <span class="absolute mr-5 mx-auto py-0 flex border w-fit pl- bg-gradient-to-r blur-xl from-teal-300 via-sky-200 to-cyan-400 bg-clip-text text-5xl box-content  text-transparent  select-none">
-                  <a class="underline decoration-blue-300"> Sebastián Bonilla </a>
-                </span> */}
-                <h1 class="sm:text-5xl text-4xl font-extrabold text-slate-200 text-color_primary ">
+                <h1 class="sm:text-5xl text-4xl font-extrabold text-slate-200 ">
                   Sebastián Bonilla
                 </h1>
-                {/* <span class="absolute mr-5 mx-auto py-0 flex border w-fit bg-gradient-to-r blur-xl from-teal-300 via-sky-200 to-cyan-400 bg-clip-text text-4xl box-content font-extrabold text-transparent  select-none">
-                  <a class="underline decoration-blue-300"> Sebastián Bonilla</a>
-                </span>
-                <span
-                  class="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
-                  Sebastián Bonilla
-                </span> */}
               </div>
-
-              {/* <h2 class="text-3xl font-bold text-slate-300 text-color_primary" >
-                  Software Developer
-                </h2> */}
-
-              {/* <div>
-                
-                <span class="absolute mr-5 x-auto py-0  font-extrabold flex border w-fit bg-gradient-to-r blur-2xl from-teal-300 via-sky-200 to-cyan-400 bg-clip-text text-4xl box-content  text-transparent  select-none">
-                  <a class="underline decoration-blue-300"> Front-End Developer </a>
-                </span>
-                <span
-                  class="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-300 via-sky-200 to-teal-300">
-                  Front-End Developer
-                </span>
-              </div> */}
 
               <div>
                 <span
-                  class="sm:text-4xl text-3xl absolute mr-6  font-extrabold text-transparent bg-clip-text blur-xl bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
+                  class="sm:text-4xl text-3xl absolute mr-6 font-extrabold text-transparent bg-clip-text blur-xl bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
                   Front-End Developer
                 </span>
                 <span
@@ -237,7 +117,7 @@ function App() {
               </div>
 
               <div>
-                <p class="text-base font-medium text-slate-300 text-color_primary max-w-2xl">
+                <p class="text-base font-medium text-slate-300 max-w-2xl">
                   Apasionado del diseño, combino habilidades de programación para crear interfaces limpias,
                   intuitivas y accesibles. Supero las expectativas de los usuarios, entregando experiencias
                   digitales atractivas y fáciles de usar.
@@ -251,21 +131,20 @@ function App() {
           {/* section PROYECTOS */}
           <div id="proyectos" class="pt-10 mb-64">
 
-            <span class="absolute mr-5 x-auto py-0  font-extrabold flex border w-fit bg-gradient-to-r blur-xl from-teal-500 via-sky-600 to-cyan-800 bg-clip-text text-xl box-content  text-transparent  select-none">
-              <a class="underline decoration-blue-300">PROYECTOS</a>
+          <span class="absolute mr-5 font-extrabold border blur-xl bg-gradient-to-r from-teal-500 via-sky-600 to-cyan-800 bg-clip-text text-xl text-transparent ">
+            PROYECTOS
             </span>
             <h2 class="text-xl font-extrabold text-slate-200 text-color_primary" >
               PROYECTOS
             </h2>
 
             {/* Proyecto 1 */}
-            <div class="flex justify-center pt-8 sm:mx-8">
+            <a class="flex justify-center pt-8 sm:mx-8" target="_blank" /* href="" */ >
               <CardProject
                 image={imgReportesPlus}
                 /* Reportes Plus */
                 tittle={
                   <>
-                    {/* <a class="text-slate-200">Reportes Plus</a> */}
                     <span
                       class="ext-base ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
                       Reportes Plus
@@ -284,37 +163,41 @@ function App() {
                 }
                 lstTech={lstTechP1}>
               </CardProject>
-            </div>
-
+            </a>
+            
             {/* Proyecto 2 */}
-            {/* <div class="flex justify-center pt-8 sm:mx-8">
+            <a class="flex justify-center pt-8 sm:mx-8" target="_blank" href="https://sebastianbonilla13.github.io/encriptador-de-texto/">
               <CardProject
-                image={imgReportesPlus}
+                image={imgEncriptadorTexto}
+
                 tittle={
                   <>
-                    <a class="text-slate-200">Reportes Plus</a>
+                    <span
+                      class="ext-base ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
+                      Encriptador de Texto
+                    </span>
                   </>
+                  
                 }
                 description={
                   <>
                     <div class="text-slate-300">
-                      Información Presupuestal Universidad del Cauca. Proporcionar a los usuarios
-                      finales del área financiera e interesados de la  universidad, una herramienta
-                      que les permita consultar y descargar  información presupuestal en tiempo real.
+                      Desarrollo de un encriptador de texto básico, que permite a los usuarios cifrar y 
+                      descifrar texto de manera sencilla. Obteniendo un resultado funcional y responsive.
                     </div>
                   </>
                 }
-                lstTech={lstTechP1}>
+                lstTech={lstTechP2}>
               </CardProject>
-            </div> */}
+            </a>
 
           </div>
 
           {/* section EXPERIENCIA */}
-          <div id="experiencia" class="pt-10 mb-28">
-
-            <span class="absolute mr-5 x-auto py-0  font-extrabold flex border w-fit bg-gradient-to-r blur-xl from-teal-500 via-sky-600 to-cyan-800 bg-clip-text text-xl box-content  text-transparent  select-none">
-              <a class="underline decoration-blue-300">EXPERIENCIA</a>
+          <div id="experiencia" class="pt-10 mb-28 ">
+            
+            <span class="absolute mr-5 font-extrabold border blur-xl bg-gradient-to-r from-teal-500 via-sky-600 to-cyan-800 bg-clip-text text-xl text-transparent ">
+              EXPERIENCIA
             </span>
             <h2 class="text-xl font-extrabold text-slate-200 text-color_primary" >
               EXPERIENCIA
@@ -325,11 +208,6 @@ function App() {
               <TableInfo
                 tittle={
                   <>
-                    {/* <a class="text-slate-200">Ingeniero de Sistemas - Universidad del Cauca</a> */}
-                    {/* <span
-                      class="absolute mr-6 ext-base ont-extrabold text-transparent bg-clip-text blur-xl bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
-                      Ingeniero de Sistemas - Universidad del Cauca
-                    </span> */}
                     <span
                       class="ext-base ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
                       Ingeniero de Sistemas - Universidad del Cauca
@@ -364,11 +242,6 @@ function App() {
               <TableInfo
                 tittle={
                   <>
-                    {/* <a class="text-slate-200">Fronted Developer - ONE (Oracle Next Education)</a> */}
-                    {/* <span
-                      class="absolute mr-6 ext-base ont-extrabold text-transparent bg-clip-text blur-xl bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
-                      Fronted Developer - ONE (Oracle Next Education)
-                    </span> */}
                     <span
                       class="ext-base ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
                       Fronted Developer - ONE (Oracle Next Education)
@@ -396,20 +269,8 @@ function App() {
           </div>
 
           {/* section ¿? */}
-          <div id="¿?" class="mt-80 mb-72 g-red-400 ">
+          <div id="¿?" class="pt-56 pb-72 ">
             <div class="py-3"> {/* text-center */}
-              {/* <h2 class="text-3xl font-bold text-slate-300 text-color_primary py-52" >
-                ¿Te interesa impulsar tu proyecto al siguiente nivel? ¡Hagámoslo realidad!
-              </h2> */}
-              {/* <h2 class="text-xl py-52 md:text-3xl font-extrabold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.blue.500),theme(colors.blue.200),theme(colors.blue.100),theme(colors.neutral.200),theme(colors.purple.200),theme(colors.purple.500))] bg-[length:100%_auto] animate-gradient">¿Te interesa impulsar tu proyecto al siguiente nivel? <a class="underline decoration-blue-300"> ¡Hagámoslo realidad!</a></h2>*/}
-
-              {/* <h1
-                class="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-600 via-slate-400 to-stone-500 ">
-
-                ¿Te interesa impulsar tu proyecto al siguiente nivel? <a class="underline decoration-blue-300"> ¡Hagámoslo realidad!</a>
-
-              </h1> */}
-
 
               <h1
                 class="sm:text-4xl text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-400 via-sky-300 to-cyan-600">
@@ -417,15 +278,12 @@ function App() {
               </h1>
               <br></br>
 
-              {/* <span class=" absolute r-5 x-auto py-0 lex order  bg-gradient-to-r lur-xl from-teal-300 via-sky-200 to-cyan-400 bg-clip-text text-4xl ox-content font-extrabold text-transparent  elect-none">
-                ¡Hagámoslo realidad!
-              </span> */}
               <span
-                class="sm:text-4xl text-2xl absolute mr-6  font-extrabold text-transparent bg-clip-text blur-xl bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
+                class="sm:text-4xl text-2xl absolute mr-6 font-extrabold text-transparent bg-clip-text blur-xl bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
                 ¡Hagámoslo realidad!
               </span>
               <span
-                class="sm:text-4xl text-2xl  font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
+                class="sm:text-4xl text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-300 via-sky-200 to-cyan-400">
                 ¡Hagámoslo realidad!
               </span>
 
@@ -433,9 +291,9 @@ function App() {
           </div>
 
           {/* section info */}
-          <div id="¿?" class="mt-96 mb-16 g-red-400 g-red-300 flex justify-center sm:px-32 ">
-            <div class="py-3"> {/* text-center */}
-              <h1 /* font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-400 via-sky-300 to-cyan-600 */
+          <div id="¿?" class="mt-16 mb-16 g-red-400 g-red-300 flex justify-center sm:px-32 ">
+            <div class="py-3">
+              <h1
                 class="m:text-base text-base text-slate-400 text-center">
                 Diseñado en Figma y codificado en Visual Studio Code. Creado con React y estilado con Tailwind CSS.
               </h1>
@@ -446,8 +304,6 @@ function App() {
       </main>
 
     </div>
-
-
 
   );
 }
